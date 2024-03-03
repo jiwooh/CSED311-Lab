0ab8ae6e8119f2ab570e57c46c5d20788241dd32
Jiwoo Hong <jiwooh@postech.ac.kr>
2024-03-03 19:12:19 +0900

    lab1.a done

--- Lab1/alu/alu.v
+++ Lab1/alu/alu.v
@@ -1,10 +1,10 @@
 `include "alu_func.v"
 
 module alu #(parameter data_width = 16) (
-	input [data_width - 1 : 0] A, 
-	input [data_width - 1 : 0] B, 
+	input signed [data_width - 1 : 0] A, 
+	input signed [data_width - 1 : 0] B, 
 	input [3 : 0] FuncCode,
-       	output reg [data_width - 1: 0] C,
+       	output reg signed [data_width - 1: 0] C,
        	output reg OverflowFlag);
 // Do not use delay in your implementation.
 
@@ -25,28 +25,76 @@ end
 */
 always @(*) begin
     case (FuncCode)
-    FUNC_ADD: begin
+    `FUNC_ADD: begin
         C = A + B; // ADD
         OverflowFlag = ((A > 0 && B > 0 && C < 0) || (A < 0 && B < 0 && C > 0)); // handle overflow
     end
-    FUNC_SUB: begin
+    `FUNC_SUB: begin
         C = A - B; // SUB
         OverflowFlag = ((A > 0 && B < 0 && C < 0) || (A < 0 && B > 0 && C > 0)); // handle overflow
     end
-    FUNC_ID: C = A; // ID
-    FUNC_NOT: C = ~A; // NOT
-    FUNC_AND: C = A & B; // AND
-    FUNC_OR: C = A | B; // OR
-    FUNC_NAND: C = ~(A & B); // NAND
-    FUNC_NOR: C = ~(A | B); // NOR
-    FUNC_XOR: C = A ^ B; // XOR
-    FUNC_XNOR: C = ~(A ^ B); // XNOR
-    FUNC_LLS: C = A << 1; // LLS
-    FUNC_LRS: C = A >> 1; // LRS
-    FUNC_ALS: C = A << 1; // ALS
-    FUNC_ARS: C = A >> 1; // ARS
-    FUNC_TCP: C = ~A + 1'b1; // TCP
-    FUNC_ZERO: C = 0; // ZERO
+    `FUNC_ID: begin
+        C = A; // ID
+        OverflowFlag = 0;
+    end
+    `FUNC_NOT: begin
+        C = ~A; // NOT
+        OverflowFlag = 0;
+    end
+    `FUNC_AND: begin
+        C = A & B; // AND
+        OverflowFlag = 0;
+    end
+    `FUNC_OR: begin
+        C = A | B; // OR
+        OverflowFlag = 0;
+    end
+    `FUNC_NAND: begin
+        C = ~(A & B); // NAND
+        OverflowFlag = 0;
+    end
+    `FUNC_NOR: begin
+        C = ~(A | B); // NOR
+        OverflowFlag = 0;
+    end
+    `FUNC_XOR: begin
+        C = A ^ B; // XOR
+        OverflowFlag = 0;
+    end
+    `FUNC_XNOR: begin
+        C = ~(A ^ B); // XNOR
+        OverflowFlag = 0;
+    end
+    `FUNC_LLS: begin
+        C = A << 1; // LLS
+        OverflowFlag = 0;
+    end
+    `FUNC_LRS: begin
+        C = A >> 1; // LRS
+        C[data_width - 1] = 0; // MSB 0
+        OverflowFlag = 0;
+    end
+    `FUNC_ALS: begin
+        C = A << 1; // ALS
+        OverflowFlag = 0;
+    end
+    `FUNC_ARS: begin
+        C = A >> 1; // ARS
+        C[data_width - 1] = C[data_width - 2]; // MSB = 2nd MSB
+        OverflowFlag = 0;
+    end
+    `FUNC_TCP: begin
+        C = ~A + 1'b1; // TCP
+        OverflowFlag = 0;
+    end
+    `FUNC_ZERO: begin
+        C = 0; // ZERO
+        OverflowFlag = 0;
+    end
+    default: begin
+        C = C; // Default
+        OverflowFlag = 0;
+    end
     endcase
 end
 
