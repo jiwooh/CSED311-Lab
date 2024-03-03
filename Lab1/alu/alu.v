db658c440c162169df92f421e8cfcf7f7d5141d4
Jiwoo Hong <jiwooh@postech.ac.kr>
2024-03-03 16:49:36 +0900

    do lab1.a

--- Lab1/alu/alu.v
+++ Lab1/alu/alu.v
@@ -23,6 +23,32 @@ end
 /*
 	YOUR ALU FUNCTIONALITY IMPLEMENTATION...
 */
+always @(*) begin
+    case (FuncCode)
+    FUNC_ADD: begin
+        C = A + B; // ADD
+        OverflowFlag = ((A > 0 && B > 0 && C < 0) || (A < 0 && B < 0 && C > 0)); // handle overflow
+    end
+    FUNC_SUB: begin
+        C = A - B; // SUB
+        OverflowFlag = ((A > 0 && B < 0 && C < 0) || (A < 0 && B > 0 && C > 0)); // handle overflow
+    end
+    FUNC_ID: C = A; // ID
+    FUNC_NOT: C = ~A; // NOT
+    FUNC_AND: C = A & B; // AND
+    FUNC_OR: C = A | B; // OR
+    FUNC_NAND: C = ~(A & B); // NAND
+    FUNC_NOR: C = ~(A | B); // NOR
+    FUNC_XOR: C = A ^ B; // XOR
+    FUNC_XNOR: C = ~(A ^ B); // XNOR
+    FUNC_LLS: C = A << 1; // LLS
+    FUNC_LRS: C = A >> 1; // LRS
+    FUNC_ALS: C = A << 1; // ALS
+    FUNC_ARS: C = A >> 1; // ARS
+    FUNC_TCP: C = ~A + 1'b1; // TCP
+    FUNC_ZERO: C = 0; // ZERO
+    endcase
+end
 
 endmodule
 
