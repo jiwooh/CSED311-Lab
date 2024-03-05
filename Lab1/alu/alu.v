1306a79a40baefab1a9aa460ace583c7eb20f571
joon363 <drogba1357!>
2024-03-05 16:34:45 +0900

    ALU Done
    지우꺼랑 비슷한데 오버플로 밖에 모듈로 뺀거정도? 사실 쓸데 없긴 할듯

--- Lab1/alu/alu.v
+++ Lab1/alu/alu.v
@@ -1,5 +1,15 @@
 `include "alu_func.v"
 
+module OverflowDetector #(parameter data_width = 16)(
+	input wire [data_width - 1 : 0] A, 
+	input wire [data_width - 1 : 0] B, 
+    wire [data_width-1:0] C,
+	output OverflowFlag);
+	assign C = A+B;
+	assign OverflowFlag = (A[data_width - 1] != B[data_width - 1]) && (A[data_width - 1] != C[data_width - 1]);
+
+endmodule
+
 module alu #(parameter data_width = 16) (
 	input [data_width - 1 : 0] A, 
 	input [data_width - 1 : 0] B, 
@@ -9,9 +19,11 @@ module alu #(parameter data_width = 16) (
 // Do not use delay in your implementation.
 
 // You can declare any variables as needed.
-/*
-	YOUR VARIABLE DECLARATION...
-*/
+wire AddOverflowDetector;
+wire SubOverflowDetector;
+OverflowDetector AddOverflow (A,B,AddOverflowDetector);
+OverflowDetector SubOverflow (A,~B,SubOverflowDetector);
+
 
 initial begin
 	C = 0;
@@ -20,9 +32,33 @@ end
 
 // TODO: You should implement the functionality of ALU!
 // (HINT: Use 'always @(...) begin ... end')
-/*
-	YOUR ALU FUNCTIONALITY IMPLEMENTATION...
-*/
+
+always @(*) begin
+	case(FuncCode)
+		FUNC_ADD: begin
+			C=A+B;
+			OverflowFlag = AddOverflowDetector;
+		end
+		FUNC_SUB: begin
+			C=A-B;
+			OverflowFlag = SubOverflowDetector;
+		end
+		FUNC_ID: C = A; // ID
+		FUNC_NOT: C = ~A; // NOT
+		FUNC_AND: C = A & B; // AND
+		FUNC_OR: C = A | B; // OR
+		FUNC_NAND: C = ~(A & B); // NAND
+		FUNC_NOR: C = ~(A | B); // NOR
+		FUNC_XOR: C = A ^ B; // XOR
+		FUNC_XNOR: C = ~(A ^ B); // XNOR
+		FUNC_LLS: C = A << 1; // LLS
+		FUNC_LRS: C = A >> 1; // LRS
+		FUNC_ALS: C = A << 1; // ALS
+		FUNC_ARS: C = A >> 1; // ARS
+		FUNC_TCP: C = ~A + 1'b1; // TCP
+		FUNC_ZERO: C = 0; // ZERO
+	endcase
+end
 
 endmodule
 
