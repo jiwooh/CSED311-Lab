51417db47f1f61dfcbd6921a2b38ca6ba5a1c42f
joon363 <drogba1357!>
2024-03-05 18:42:29 +0900

    ALU TB All pass (with some updates)
    오버플로 문제 수정, 스위치케이스에서 모든 곳에 overflow=0 넣을 필요 없음

--- Lab1/alu/alu.v
+++ Lab1/alu/alu.v
@@ -1,15 +1,5 @@
 `include "alu_func.v"
 
-module OverflowDetector #(parameter data_width = 16)(
-	input wire [data_width - 1 : 0] A, 
-	input wire [data_width - 1 : 0] B, 
-    wire [data_width-1:0] C,
-	output OverflowFlag);
-	assign C = A+B;
-	assign OverflowFlag = (A[data_width - 1] != B[data_width - 1]) && (A[data_width - 1] != C[data_width - 1]);
-
-endmodule
-
 module alu #(parameter data_width = 16) (
 	input [data_width - 1 : 0] A, 
 	input [data_width - 1 : 0] B, 
@@ -19,11 +9,10 @@ module alu #(parameter data_width = 16) (
 // Do not use delay in your implementation.
 
 // You can declare any variables as needed.
-wire AddOverflowDetector;
-wire SubOverflowDetector;
-OverflowDetector AddOverflow (A,B,AddOverflowDetector);
-OverflowDetector SubOverflow (A,~B,SubOverflowDetector);
-
+// You can declare any variables as needed.
+/*
+	YOUR VARIABLE DECLARATION...
+*/
 
 initial begin
 	C = 0;
@@ -34,29 +23,36 @@ end
 // (HINT: Use 'always @(...) begin ... end')
 
 always @(*) begin
+	OverflowFlag = 0;
 	case(FuncCode)
-		FUNC_ADD: begin
+		`FUNC_ADD: begin
 			C=A+B;
-			OverflowFlag = AddOverflowDetector;
+			OverflowFlag = (A[data_width - 1] == B[data_width - 1]) && (A[data_width - 1] != C[data_width - 1]);
 		end
-		FUNC_SUB: begin
+		`FUNC_SUB: begin
 			C=A-B;
-			OverflowFlag = SubOverflowDetector;
+			OverflowFlag = (A[data_width - 1] != B[data_width - 1]) && (A[data_width - 1] != C[data_width - 1]);
 		end
-		FUNC_ID: C = A; // ID
-		FUNC_NOT: C = ~A; // NOT
-		FUNC_AND: C = A & B; // AND
-		FUNC_OR: C = A | B; // OR
-		FUNC_NAND: C = ~(A & B); // NAND
-		FUNC_NOR: C = ~(A | B); // NOR
-		FUNC_XOR: C = A ^ B; // XOR
-		FUNC_XNOR: C = ~(A ^ B); // XNOR
-		FUNC_LLS: C = A << 1; // LLS
-		FUNC_LRS: C = A >> 1; // LRS
-		FUNC_ALS: C = A << 1; // ALS
-		FUNC_ARS: C = A >> 1; // ARS
-		FUNC_TCP: C = ~A + 1'b1; // TCP
-		FUNC_ZERO: C = 0; // ZERO
+		`FUNC_ID: C = A; // ID
+		`FUNC_NOT: C = ~A; // NOT
+		`FUNC_AND: C = A & B; // AND
+		`FUNC_OR: C = A | B; // OR
+		`FUNC_NAND: C = ~(A & B); // NAND
+		`FUNC_NOR: C = ~(A | B); // NOR
+		`FUNC_XOR: C = A ^ B; // XOR
+		`FUNC_XNOR: C = ~(A ^ B); // XNOR
+		`FUNC_LLS: C = A << 1; // LLS
+		`FUNC_LRS: begin
+        	C = A >> 1; // LRS
+        	C[data_width - 1] = 0; // MSB 0
+		end
+		`FUNC_ALS: C = A << 1; // ALS
+		`FUNC_ARS: begin
+        	C = A >> 1; // ARS
+        	C[data_width - 1] = C[data_width - 2]; // MSB = 2nd MSB
+		end
+		`FUNC_TCP: C = ~A + 1'b1; // TCP
+		`FUNC_ZERO: C = 0; // ZERO
 	endcase
 end
 
