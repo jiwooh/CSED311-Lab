f8a48560322c690e5fc7b50a31b1bf2372794e8c
joon363 <drogba1357!>
2024-05-14 01:20:03 +0900

    recursive, loop 빼고 성공

--- Lab4-2/InstMemory.v
+++ Lab4-2/InstMemory.v
@@ -22,7 +22,11 @@ module InstMemory #(parameter MEM_DEPTH = 1024) (input reset,
           /* verilator lint_on BLKSEQ */
           // DO NOT TOUCH COMMENT ABOVE
       // Provide path of the file including instructions with binary format
-      $readmemh("./student_tb/basic_mem.txt", mem);
+      // $readmemh("./student_tb/basic_mem.txt", mem); //pass
+      //  $readmemh("./student_tb/ifelse_mem.txt", mem); //pass
+      // $readmemh("./student_tb/loop_mem.txt", mem); //TODO: fail
+      // $readmemh("./student_tb/non-controlflow_mem.txt", mem); //pass
+      $readmemh("./student_tb/recursive_mem.txt", mem); //TODO: fail
     end
   end
 
