b6468ccff58a208c916231e7c2c40451e70c9f2d
joon363 <drogba1357!>
2024-05-15 08:42:16 +0900

    보고서까지 작성후 제출 완료

--- Lab4-2/BTB.v
+++ Lab4-2/BTB.v
@@ -23,21 +23,19 @@ module BTB (
     // input query
     wire [31:0] query_tag;
     wire [4:0]  query_idx;
+    assign query_tag = pc[31:0];
+    assign query_idx = pc[6:2] ^ BHSR;
     // real pc
     wire [4:0]  real_pc_idx;
     wire [31:0] real_pc_tag;
-    wire taken;
-
-    assign query_tag = pc[31:0];
-    assign query_idx = pc[6:2] ^ BHSR;
-
     assign real_pc_tag = real_pc[31:0];
     assign real_pc_idx = real_pc[6:2] ^ real_pc_BHSR;
 
+    wire taken;
+    assign taken = (branch & alu_bcond) | is_jal | is_jalr;
+
     reg [4:0] BHSR_tmp;
     assign BHSR = (BHSR_tmp << 1) + {4'b0, taken};
-
-    assign taken = (branch & alu_bcond) | is_jal | is_jalr;
 
     reg [31:0] dest; // temporary wire for convenience
 
