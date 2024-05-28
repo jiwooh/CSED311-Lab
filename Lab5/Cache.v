5eb9e76b64b4c123fefd10785e8c8f7302d6af20
joon363 <drogba1357!>
2024-05-29 05:24:45 +0900

    formmtting

--- Lab5/Cache.v
+++ Lab5/Cache.v
@@ -31,25 +31,23 @@ module Cache #(parameter LINE_SIZE = 16//,
 
   wire [127:0] dmem_output_set;
   wire [127:0] bank_output_set;
-  wire [127:0] bank_output_set_1;
   wire [31:0] bank_output_line;
-  wire [31:0] bank_output_line_1;
-  wire [31:0] bank_write_back_addr_1;
+  wire [31:0] bank_write_back_addr;
   wire [31:0] dmem_addr;
-  wire bank_is_hit_1;
-  wire bank_dmem_read_1;
-  wire bank_dmem_write_1;
+  wire bank_is_hit;
+  wire bank_dmem_read;
+  wire bank_dmem_write;
   wire data_write_back_complete;
-  wire [1:0] bank_state_1;
-  assign is_ready = (bank_state_1==`CACHE_IDLE);
-  assign is_hit = bank_is_hit_1;
-  assign dout = bank_output_line_1;
-  assign bank_output_set = bank_output_set_1;
+  wire [1:0] bank_state;
+  assign is_ready = (bank_state==`CACHE_IDLE);
+  assign is_hit = bank_is_hit;
+  assign dout = bank_output_line;
+  //assign bank_output_set = bank_output_set_1;
   assign dmem_addr = 
-    (bank_state_1==`CACHE_WRITE_BACK_REQUEST) 
-    ? bank_write_back_addr_1 : addr;
+    (bank_state==`CACHE_WRITE_BACK_REQUEST) 
+    ? bank_write_back_addr : addr;
   assign data_write_back_complete = 
-    (bank_state_1==`CACHE_WRITE_BACK_REQUEST) 
+    (bank_state==`CACHE_WRITE_BACK_REQUEST) 
     && is_data_mem_ready
     && counter > 5;
   
@@ -65,13 +63,13 @@ module Cache #(parameter LINE_SIZE = 16//,
     .data_write_back_complete(data_write_back_complete),
     .input_set(dmem_output_set), //128 bit
     .input_line(din), //32 bit
-    .output_set(bank_output_set_1), //128 bit
-    .output_line(bank_output_line_1), // 32 bit
-    .output_addr(bank_write_back_addr_1), // 32 bit
-    .dmem_read(bank_dmem_read_1),
-    .dmem_write(bank_dmem_write_1),
-    .cache_state(bank_state_1),
-    .is_hit(bank_is_hit_1)
+    .output_set(bank_output_set), //128 bit
+    .output_line(bank_output_line), // 32 bit
+    .output_addr(bank_write_back_addr), // 32 bit
+    .dmem_read(bank_dmem_read),
+    .dmem_write(bank_dmem_write),
+    .cache_state(bank_state),
+    .is_hit(bank_is_hit)
   );
 
   // Instantiate data memory
@@ -81,8 +79,8 @@ module Cache #(parameter LINE_SIZE = 16//,
 
     .is_input_valid(is_input_valid && request_counter<5),
     .addr((dmem_addr>>(`CLOG2(LINE_SIZE)))),        // NOTE: address must be shifted by CLOG2(LINE_SIZE)
-    .mem_read(bank_dmem_read_1),
-    .mem_write(bank_dmem_write_1),
+    .mem_read(bank_dmem_read),
+    .mem_write(bank_dmem_write),
     .din(bank_output_set),
 
     // is output from the data memory valid?
@@ -99,11 +97,11 @@ module Cache #(parameter LINE_SIZE = 16//,
         request_counter <= 0;
       end
       else begin
-        if(bank_state_1==`CACHE_WRITE_BACK_REQUEST&& is_input_valid) begin
+        if(bank_state==`CACHE_WRITE_BACK_REQUEST&& is_input_valid) begin
           counter <= counter + 1;
           request_counter <= request_counter+1;
         end 
-        else if(bank_state_1==`CACHE_WRITE_ALLOCATE_REQUEST&& is_input_valid) begin
+        else if(bank_state==`CACHE_WRITE_ALLOCATE_REQUEST&& is_input_valid) begin
           counter <=0;
           request_counter <= request_counter+1;
         end
